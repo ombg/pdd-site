3ce22917ec69c6e105ddaa6edb4bcd35775907ca
Oliver M <olliwaa@gmail.com>
2020-07-20 21:21:22 -0700

    Adding user creation code plus unit tests.

---
+++ app/user/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import UserSerializer, AuthTokenSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for the user"""
+    serializer_class = AuthTokenSerializer
+    # This call enables the browser view.
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
